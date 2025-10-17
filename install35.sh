abaaad3103140b0f56cdc295c91f97092b2e61dd
Jens <51135178+katzenjens@users.noreply.github.com>
2025-10-17 19:12:26 +0200

    Update install35.sh

--- install35.sh
+++ install35.sh
@@ -4,7 +4,7 @@ sudo cp -rf waveshare35a.dtbo /boot/overlays/waveshare35a.dtbo
 sudo cp -rf 99-fbdev.conf /usr/share/X11/xorg.conf.d/99-fbdev.conf
 sudo cp -rf 45-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
 sudo cp -rf 99-calibration.conf /usr/share/X11/xorg.conf.d/99-calibration.conf
-sudo cp -rf config.txt /boot/firmware/config.txt
+sudo cp -rf config35.txt /boot/firmware/config.txt
 sudo cp -rf cmdline.txt /boot/firmware/cmdline.txt
 sudo cp -rf rc.local /etc/rc.local
 sudo cp -rf console-setup /etc/default/console-setup
